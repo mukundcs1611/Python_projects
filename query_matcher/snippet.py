4e0151b01041946322ed7722b81b1f3f41e2cdbb
Mukund <srinivasmukund.chavali@mavs.uta.edu>
2017-10-25 17:00:52 +0530

    Not necessary

--- query_matcher/snippet.py
+++
@@ -1,7 +0,0 @@
-import os
-corpusroot = './presidential_debates'
-for filename in os.listdir(corpusroot):
-    file = open(os.path.join(corpusroot, filename), "r", encoding='UTF-8')
-    doc = file.read()
-    file.close() 
-    doc = doc.lower()
